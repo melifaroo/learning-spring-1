f5f0195b76f443836b69c9faf21533ab23c5ba28
melifaroo <den.volgin@gmail.com>
2023-07-20 23:44:57 +0200

    init spring boot

---
+++ src/test/java/com/github/melifaroo/learning/spring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.melifaroo.learning.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
